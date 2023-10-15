cbdfcfedd3a494ee6d5c1b7f53079e991fd19e55
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2023-10-15 14:50:45 -0300

    Create s.js

---
+++ s.js
@@ -0,0 +1,16 @@
+function dividirNome() {
+    var nomeCompleto = document.getElementById("nomeCompleto").value;
+    var nomeArray = nomeCompleto.split("");
+    
+    if (nomeArray.length >= 1) {
+        document.getElementById("nome").value = nomeArray[0];
+        if (nomeArray.length >= 2) {
+            document.getElementById("sobrenome").value = nomeArray.slice(1).join(" ");
+        } else {
+            document.getElementById("sobrenome").value = "";
+        }
+    } else {
+        alert("Por favor, insira um nome completo válido.");
+    }
+
+}
