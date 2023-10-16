43421c9f130e03c2ebf67622c53f7c5d6a107534
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2023-10-16 09:03:15 -0300

    Create parag.js

---
+++ parag.js
@@ -0,0 +1,8 @@
+function js_style() {
+    // aqui pega o elemento do parágrafo
+    var paragraph = document.getElementById('text');
+
+    paragraph.style.fontFamily = 'Arial, sans-serif'; // Fonte
+    paragraph.style.fontSize = '30px'; // Tamanho 
+    paragraph.style.color = 'red'; // Cor 
+}
